35dcc501e1bf0abb0c791eb956fad04cfd625170
Marcelo Sercheli <msercheli@gmail.com>
2024-01-25 17:24:20 -0500

    Clean

--- package.py
+++ package.py
@@ -9,19 +9,8 @@ description = \
     """
 
 with scope("config") as c:
-    # Determine location to release: internal (int) vs external (ext)
-
-    # NOTE: Modify this variable to reflect the current package situation
-    release_as = "ext"
-
-    # The `c` variable here is actually rezconfig.py
-    # `release_packages_path` is a variable defined inside rezconfig.py
-
     import os
-    if release_as == "int":
-        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_INT"]
-    elif release_as == "ext":
-        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
+    c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
 
 requires = [
     "libtiff-4.0.7",
